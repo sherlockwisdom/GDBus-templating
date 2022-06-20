88957a9d87fa1a47c23e1d4b1fd0f6089ff2eacd
sherlock wisdom <wisdomnji@gmail.com>
2022-06-20 03:54:33 +0100

    update: fetching dict data from dbus object

--- main.cxx
+++ main.cxx
@@ -53,7 +53,9 @@ void compose_message(GDBusConnection *connection, GError* error,
 			interface_,
 			method);
 
-	GVariant* boolean_enable = g_variant_new("(b)", 1);
+	// https://docs.gtk.org/glib/gvariant-format-strings.html
+	// GVariant* boolean_enable = g_variant_new("(b)", 1);
+	GVariant* boolean_enable = NULL;
 	::g_dbus_message_set_body(
 			g_dbus_message, 
 			boolean_enable);
@@ -75,6 +77,46 @@ void compose_message(GDBusConnection *connection, GError* error,
 		return;
 	}
 	std::cout << method << " method called successfully" << std::endl;
+
+	GDBusMessageType g_dbus_message_type = g_dbus_message_get_message_type(g_dbus_message);
+	switch(g_dbus_message_type) {
+		case G_DBUS_MESSAGE_TYPE_INVALID:
+			std::cout << "Message = Invalid" << std::endl;
+			break;
+		case G_DBUS_MESSAGE_TYPE_METHOD_CALL:
+			std::cout << "Message = Method call" << std::endl;
+			break;
+		case G_DBUS_MESSAGE_TYPE_METHOD_RETURN:
+			std::cout << "Message = Method return" << std::endl;
+			break;
+		case G_DBUS_MESSAGE_TYPE_ERROR:
+			std::cout << "Message = Error" << std::endl;
+			break;
+		case G_DBUS_MESSAGE_TYPE_SIGNAL:
+			std::cout << "Message = Signal" << std::endl;
+			break;
+		default:
+			std::cout << "Fuck!" << std::endl;
+	}
+	
+	
+	// https://developer-old.gnome.org/gio/stable/GDBusMessage.html#g-dbus-message-print
+	std::cout << g_dbus_message_print(g_dbus_message, 1) << std::endl;
+
+
+	const gchar* result;
+	GVariantIter *iter;
+	g_variant_get(g_dbus_message_get_body(g_dbus_message), "(a{oa{sa{sv}}})", &iter);
+	while (g_variant_iter_loop (iter, "{oa{sa{sv}}}", &result)) {
+		// g_print("%s\n", result);
+	}
+
+	// std::cout << result << std::endl;
+
+
+
+
+
 	/*
 	const char *g_dbus_result;
 	::g_dbus_message_get_args(
@@ -97,10 +139,44 @@ void compose_message(GDBusConnection *connection, GError* error,
 	::g_object_unref(g_dbus_message);
 }
 
+
+void callbackFunction(GObject* source_object, GAsyncResult* res, gpointer user_data) {
+	GError* error = NULL;
+	GDBusObjectManager* g_dbus_object_manager = ::g_dbus_object_manager_client_new_finish( res, &error);
+	if(error) {
+		gchar* error_message = g_dbus_error_get_remote_error(error);
+		std::cerr << error_message << std::endl;
+
+		g_error_free(error);
+		return;
+	}
+
+	std::cout << "* new object manager client created..." << std::endl;
+}
+
+
+void get_interfaces(GDBusConnection *connection, const gchar* path, const gchar* name, GError* error) {
+	g_dbus_object_manager_client_new( 
+			connection,
+			G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_DO_NOT_AUTO_START,
+			name,
+			path,
+			NULL,
+			NULL,
+			NULL,
+			NULL,
+			callbackFunction,
+			NULL);
+}
+
+
 int main(int argc, gchar** argv) {
 
 	gchar* path;
 	gchar* method;
+	const gchar* connection_bus_name = "org.freedesktop.ModemManager1";
+	// const gchar* interface_ = "org.freedesktop.ModemManager1.Modem";
+	const gchar* interface_ = "org.freedesktop.DBus.ObjectManager";
 
 	for(int i=1; i<argc;++i) {
 		if(std::string(argv[i]) == "--method") {
@@ -142,8 +218,6 @@ int main(int argc, gchar** argv) {
 	std::cout << "Connected to D-Bus as -> " << 
 		bus_unique_name << std::endl;
 
-	const gchar* connection_bus_name = "org.freedesktop.ModemManager1";
-	const gchar* interface_ = "org.freedesktop.ModemManager1.Modem";
 
 	std::cout << "- Connection Bus Name: " << 
 		connection_bus_name << std::endl;
@@ -151,6 +225,9 @@ int main(int argc, gchar** argv) {
 	std::cout << "- Path: " << path << std::endl;
 	std::cout << "- Method: " << method << std::endl;
 
+	// get_interfaces(connection, path, connection_bus_name, error);
+
+	
 	compose_message(connection, error, 
 			connection_bus_name, 
 			path, 
