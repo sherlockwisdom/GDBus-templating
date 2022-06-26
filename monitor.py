5b2081eedce9f4d9bab9aa9ab127099555a31b9c
sherlock wisdom <wisdomnji@gmail.com>
2022-06-26 23:24:16 +0100

    update: added signals for modems properties

--- monitor.py
+++ monitor.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python3
 
-
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 
 
+interface_added_str = "InterfacesAdded"
+interface_removed_str = "InterfacesRemoved"
 
-DBusGMainLoop(set_as_default=True)
-loop = GLib.MainLoop()
-bus = dbus.SystemBus()
+def modem_property_changed(*args, **kwargs):
+    print("Modem property changed...")
+    print(args, '\n')
+    print(kwargs, '\n\n')
 
-name = 'org.freedesktop.ModemManager1'
-path = '/org/freedesktop/ModemManager1'
-interface = 'org.freedesktop.DBus.ObjectManager'
 
-dbus_modem_manager = bus.get_object(name, path, True)
-
-dbus_interface_obj_mnger = dbus.Interface(dbus_modem_manager, dbus_interface=interface)
-
-def modem_added(modem_path):
+def modem_added(modem_path, *args):
     """
     """
     print("- modem added: %s" % (modem_path))
-    dbus_interface_obj_mnger = dbus.Interface(dbus_modem_manager, dbus_interface=interface)
+
+    dbus_modem = bus.get_object(name, modem_path, True)
+
+    modem_interface = 'org.freedesktop.ModemManager1.Modem.Modem3gpp'
+    modem_dbus_props_iface = 'org.freedesktop.DBus.Properties'
+
+
+    props = dbus.Interface(dbus_modem, dbus_interface=modem_dbus_props_iface)
 
     """
-    dbus_interface_obj_mnger.connect_to_signal(
-            "InterfacesRemoved", 
-            handler_function=handler_function_interfaces_changed, 
+    print(props.GetAll(modem_interface))
+    print("Operator code:", props.Get(modem_interface, "OperatorCode"))
+    print("Operator name:", props.Get(modem_interface, "OperatorName"))
+    """
+
+    props.connect_to_signal(
+            "PropertiesChanged",
+            handler_function=modem_property_changed,
             path_keyword='path', 
             member_keyword='member', 
             interface_keyword='interface', 
             destination_keyword='destination',
             sender_keyword='sender')
-    """
 
 
 def modem_removed(modem_path):
     """
     """
     print("- modem removed: %s" % (modem_path))
-
-
-interface_added_str = "InterfacesAdded"
-interface_removed_str = "InterfacesRemoved"
 
 
 def handler_function_interfaces_changed(*args, **kwargs):
@@ -64,22 +66,41 @@ def handler_function_interfaces_changed(*args, **kwargs):
         modem_removed(modem_path)
 
 
-dbus_interface_obj_mnger.connect_to_signal(
-        "InterfacesRemoved", 
-        handler_function=handler_function_interfaces_changed, 
-        path_keyword='path', 
-        member_keyword='member', 
-        interface_keyword='interface', 
-        destination_keyword='destination',
-        sender_keyword='sender')
+if __name__ == '__main__':
+    DBusGMainLoop(set_as_default=True)
+    loop = GLib.MainLoop()
+    bus = dbus.SystemBus()
 
-dbus_interface_obj_mnger.connect_to_signal(
-        "InterfacesAdded", 
-        handler_function=handler_function_interfaces_changed, 
-        path_keyword='path', 
-        member_keyword='member', 
-        interface_keyword='interface', 
-        destination_keyword='destination',
-        sender_keyword='sender')
+    name = 'org.freedesktop.ModemManager1'
+    path = '/org/freedesktop/ModemManager1'
+    interface = 'org.freedesktop.DBus.ObjectManager'
 
-loop.run()
+    dbus_mm = bus.get_object(name, path, True)
+
+    mm_iface_obj_mng = dbus.Interface(dbus_mm, dbus_interface=interface)
+
+    managed_objects : dict = mm_iface_obj_mng.GetManagedObjects()
+
+    for object_path, _ in managed_objects.items():
+        print("- connected: %s" % (object_path))
+        modem_added(object_path)
+
+    mm_iface_obj_mng.connect_to_signal(
+            "InterfacesRemoved", 
+            handler_function=handler_function_interfaces_changed, 
+            path_keyword='path', 
+            member_keyword='member', 
+            interface_keyword='interface', 
+            destination_keyword='destination',
+            sender_keyword='sender')
+
+    mm_iface_obj_mng.connect_to_signal(
+            "InterfacesAdded", 
+            handler_function=handler_function_interfaces_changed, 
+            path_keyword='path', 
+            member_keyword='member', 
+            interface_keyword='interface', 
+            destination_keyword='destination',
+            sender_keyword='sender')
+
+    loop.run()
